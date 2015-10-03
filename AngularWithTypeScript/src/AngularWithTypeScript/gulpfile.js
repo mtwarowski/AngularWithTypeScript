6f3f0132270248f157cdcbbe35d4f1eb9f6633e9
serekmt <serkemt@wp.pl>
2015-10-03 02:06:53 +0200

    Scelet of app

---
+++ AngularWithTypeScript/src/AngularWithTypeScript/gulpfile.js
@@ -0,0 +1,35 @@
+﻿
+var gulp = require('gulp'),
+    tsc = require("gulp-typescript"),
+    watch = require('gulp-watch'),
+    del = require("del"),
+    uglify = require('gulp-uglify')
+    project = require("./project.json");
+
+
+var paths = {
+    webroot: "./" + project.webroot + "/",
+    scripts: "./" + project.webroot + "/scripts/",
+    ts: './webApp/ts/**/*.ts',
+    tslib: "./webApp/tslib/"
+};
+
+gulp.task('ts:build', function () {
+    gulp.src([paths.ts])
+    .pipe(tsc().js)
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts));
+});
+
+gulp.task('ts:clean', function () {
+    del([paths.scripts]);
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.ts, ['ts:clean', 'ts:build']);
+});
+
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
---
+++ AngularWithTypeScript/src/AngularWithTypeScript/webApp/ts/hello.ts
@@ -0,0 +1,4 @@
+﻿function sayHello() {
+    alert("hello");
+
+}
